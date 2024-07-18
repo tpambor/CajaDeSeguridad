8422c241abaf8e4d76cf7fa0dc96ce759762e087
Tim Pambor <t.pambor@uniandes.edu.co>
2024-07-18 17:40:12 +0200

    CI: Subir imagenes a ECR (#6)
    Subir docker contenedortes a ECR

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "ecr_url" {
   value       = aws_ecr_repository.repository.repository_url
   description = "The ECR repository URL"
 }
+
+output "aws_region" {
+  value       = local.region
+  description = "AWS region"
+}
