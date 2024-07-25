12a886b96e9bba648af1e68d1a02e55a3610321a
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-07-24 19:38:01 -0500

    Service

---
+++ front/src/app/element-list/elemento.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { Elemento } from './elemento';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ElementoService {
+
+private apiUrl = environment.baseUrl + 'api/caja/secretos';
+constructor(private http: HttpClient) { }
+
+getElementos(): Observable<Elemento[]> {
+  return this.http.get<Elemento[]>(this.apiUrl);
+}
+
+}
