3568871694272fe02cdffa0786efb71c21e1670b
Tim Pambor <t.pambor@uniandes.edu.co>
2024-07-27 17:59:11 +0200

    Limpieza

--- front/src/app/toast/toast.module.ts
+++ front/src/app/toast/toast.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ToastService } from './toast.service';
 import { ToasterComponent } from './toaster/toaster.component';
 import { ToastComponent } from './toast/toast.component';
 import { HttpClientModule } from '@angular/common/http';
